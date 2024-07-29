067d169dff8a77d780ac7356e7dcb34e5edde8f7
LeoPazEs <estevam.leonardopaz@gmail.com>
2024-07-29 16:18:08 -0300

    defer enviroment evaluated immediately
    Signed-off-by: LeoPazEs <estevam.leonardopaz@gmail.com>

--- function-methods-interfaces-go/week-2/variadicAndDeffer/variadicAndDeffer.go
+++ function-methods-interfaces-go/week-2/variadicAndDeffer/variadicAndDeffer.go
@@ -15,5 +15,6 @@ func getMax(vals ...int) int {
 func main() {
 	vSlice := []int{1, 2, 3, 4, 5}
 	defer fmt.Println(getMax(vSlice...))
+	vSlice = append(vSlice, 6)
 	fmt.Println(getMax(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15))
 }
